e4ab7a93bcdc6d97325b899a6fb88c838ff46b90
hettipower <hettipower@gmail.com>
2019-10-08 16:43:32 +0530

    Create Featured vendor component

---
+++ src/components/featured-vendor/featured-vendor.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './featured-vendor.styles.scss';
+
+class FeaturedVendor extends React.Component {
+    constructor(){
+        super();
+
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <div className="sectionWrap" id="featuredVendorWrap">
+                <div className="clipbg"/>
+            </div>
+        )
+    }
+}
+
+export default FeaturedVendor;
--- src/pages/homepage/home.components.jsx
+++ src/pages/homepage/home.components.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import './home.styles.scss';
 
 import MainBanner from '../../components/main-banner/main-banner.component';
+import FeaturedVendor from '../../components/featured-vendor/featured-vendor.component';
 
 const HomePage = () => (
     <div className="homepage">
         <MainBanner/>
+        <FeaturedVendor />
     </div>
 );
 
