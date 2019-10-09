e76739bd2d6dc7060320d48dcff31a6571e67e5b
hettipower <hettipower@gmail.com>
2019-10-09 19:20:29 +0530

    Update routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch , Route } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,12 +9,18 @@ import HomePage from './pages/homepage/home.components';
 import Header from './components/header/header.component';
 import Footer from './components/footer/footer.component';
 
+const BaseURL = ( process.env.NODE_ENV === 'development' ) ? '/' : process.env.PUBLIC_URL ;
+
+export const history = createBrowserHistory({
+  basename: BaseURL
+});
+
 function App() {
   return (
     <div>
       <Header/>
       <Switch>
-        <Route exact path="/" component={HomePage} />
+        <Route exact path={`${BaseURL}`} component={HomePage} />
       </Switch>
       <Footer/>
     </div>
