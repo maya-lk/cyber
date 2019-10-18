224a23d206d0c6bbd1376a5c045feb6dd3e65cf5
hettipower <hettipower@gmail.com>
2019-10-18 15:59:14 +0530

    Update Service Page

--- src/pages/servicepage/servicepage.component.jsx
+++ src/pages/servicepage/servicepage.component.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import loadable from '@loadable/component';
+import Container from 'react-bootstrap/Container';
 
 import './servicepage.styles.scss';
+
+import ServiceBannerImage from '../../assets/images/service-banner-image.png';
 
 const Footer = loadable(() => import('../../components/footer/footer.component'), {
     fallback: <div>Loading...</div>,
@@ -19,7 +22,19 @@ class ServicePage extends React.Component {
     render() {
         return (
             <div className="servicesPage">
-                Services
+                <div className="servicesBannerWrap">
+                    <Container className="d-flex">
+                        <div className="textWrap">
+                            <h1>Having Sudden cravings?</h1>
+                            <h3>we got your back, we have <span>150+ Resturants</span> to select from</h3>
+                            <a href="#" className="btn">SHOW ME THE FOOD</a>
+                        </div>
+                        <div className="imageWrap">
+                            <img src={ServiceBannerImage} alt="Service Banner" />
+                        </div>
+                        <h2 className="bigText">FOOD</h2>
+                    </Container>
+                </div>
                 <Footer/>
             </div>
         )
